28ce1b9c4040368ecb5eb4b4dbeb63fe1ba33bdb
HassanSaleban <salebanali@outlook.com>
2025-05-23 10:58:39 +0200

    mise à jour partie2

--- partie2.py
+++ partie2.py
@@ -4,12 +4,6 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import plotly.express as px
 
-import streamlit as st
-import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
-import plotly.express as px
-
 
 # importation des datasets depuis GitHub
 
